32a47a8c660b9311d36de9e871bbd2a1760297fe
phulicon1998 <phulicon815490@gmail.com>
2019-01-29 10:39:08 +0700

    Config for deploying

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const publicPath = path.join(__dirname, "../public");
+const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => console.log("Server is running on port 3000"));
+app.listen(port, () => console.log(`Server is running on port ${port}`));
