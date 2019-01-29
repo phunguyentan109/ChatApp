812feeb6cb3bc0afb3ea95b315c4801e820a8ab9
phulicon1998 <phulicon815490@gmail.com>
2019-01-29 10:29:28 +0700

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,8 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+const publicPath = path.join(__dirname, "../public");
+
+app.use(express.static(publicPath));
+
+app.listen(3000, () => console.log("Server is running on port 3000"));
